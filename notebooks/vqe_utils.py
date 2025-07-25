395c95f600f4a7a9ba995196610f4bd52570b593
Sid Richards <siddrichards@hotmail.co.uk>
2025-07-25 22:23:52 +0000

    tweaks

---
+++ notebooks/vqe_utils.py
@@ -0,0 +1,63 @@
+import pennylane as qml
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Define the Ansäzes
+def uccsd_ansatz(params, wires):
+    qml.RY(params[0], wires=wires[0])
+    qml.CNOT(wires=[wires[0], wires[1]])
+    qml.RY(-params[0], wires=wires[1])
+    qml.CNOT(wires=[wires[0], wires[1]])
+
+
+def ry_cz_ansatz(params, wires):
+    for i in range(len(wires)):
+        qml.RY(params[i], wires=wires[i])
+    for i in range(len(wires) - 1):
+        qml.CZ(wires=[wires[i], wires[i + 1]])
+
+
+def minimal_ansatz(params, wires):
+    qml.RY(params[0], wires=wires[0])
+    qml.CNOT(wires=[wires[0], wires[1]])
+    
+# Create list of Ansätzes
+ANSATZES = {
+    "UCCSD": uccsd_ansatz,
+    "RY-CZ": ry_cz_ansatz,
+    "Minimal": minimal_ansatz,
+}
+
+
+# Define VQE circuit function
+def create_vqe_circuit(ansatz_fn, hamiltonian, dev, wires):
+    @qml.qnode(dev)
+    def circuit(params):
+        ansatz_fn(params, wires=wires)
+        return qml.expval(hamiltonian)
+    return circuit
+
+
+# Initial parameter function
+def init_params(ansatz_name, num_wires):
+    if ansatz_name in ["UCCSD", "Minimal"]:
+        return 0.01 * np.random.randn(1)
+    elif ansatz_name == "RY-CZ":
+        return 0.01 * np.random.randn(num_wires)
+    else:
+        raise ValueError("Unknown ansatz")
+
+
+# Function that runs the VQE algorithm for a given optimizer and ansätz
+def run_vqe(cost_fn, init_params, optimizer, stepsize=0.2, max_iters=50):
+    opt = optimizer(stepsize) if isinstance(stepsize, float) else optimizer()
+    params = init_params
+    energies = []
+
+    for _ in range(max_iters):
+        params = opt.step(cost_fn, params)
+        energy = cost_fn(params)
+        energies.append(energy)
+
+    return params, energies
