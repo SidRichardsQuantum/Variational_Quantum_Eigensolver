706e2b3d0048cc6bcc9ae782dadde604cd51252c
Sid Richards <siddrichards@hotmail.co.uk>
2025-10-26 16:43:53 +0000

    clean-ups

---
+++ vqe/__init__.py
@@ -0,0 +1,51 @@
+from .core import (
+    run_vqe,
+    run_vqe_noise_sweep,
+    run_vqe_optimizer_comparison,
+    run_vqe_ansatz_comparison,
+    run_vqe_multi_seed_noise,
+    run_vqe_geometry_scan,
+    run_vqe_mapping_comparison,
+)
+from .ansatz import get_ansatz, init_params, ANSATZES
+from .optimizer import minimize_energy, get_optimizer
+from .hamiltonian import build_hamiltonian, generate_geometry
+from .io_utils import (
+    make_run_config_dict,
+    run_signature,
+    save_run_record,
+    ensure_dirs,
+)
+from .visualize import (
+    plot_convergence,
+    plot_noise_sweep,
+    plot_optimizer_comparison,
+    plot_ansatz_comparison,
+    plot_noise_statistics,
+)
+
+__all__ = [
+    "run_vqe",
+    "run_vqe_noise_sweep",
+    "run_vqe_optimizer_comparison",
+    "run_vqe_ansatz_comparison",
+    "run_vqe_multi_seed_noise",
+    "run_vqe_geometry_scan",
+    "run_vqe_mapping_comparison",
+    "get_ansatz",
+    "init_params",
+    "ANSATZES",
+    "minimize_energy",
+    "get_optimizer",
+    "build_hamiltonian",
+    "generate_geometry",
+    "make_run_config_dict",
+    "run_signature",
+    "save_run_record",
+    "ensure_dirs",
+    "plot_convergence",
+    "plot_noise_sweep",
+    "plot_optimizer_comparison",
+    "plot_ansatz_comparison",
+    "plot_noise_statistics",
+]
